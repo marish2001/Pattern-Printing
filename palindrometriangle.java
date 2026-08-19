0686088967db6550395e4909452072ae8a2c724d
M ANBU MARISH <113194705+marish2001@users.noreply.github.com>
2026-08-19 06:47:24 -0700

    Create palindrometriangle.java

---
+++ palindrometriangle.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+public class patternprinting 
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\n Enter the number");
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print("  ");
+            }
+            for(int j=i;j>=1;j--)
+            {
+                System.out.print(j+ " ");
+            }
+            for(int j=2;j<=i;j++)
+            {
+                System.out.print(j+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
