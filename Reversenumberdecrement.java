cd2957acc0b27f474b34c1eb3a8bac02be6600fd
M ANBU MARISH <113194705+marish2001@users.noreply.github.com>
2026-08-19 02:16:12 -0700

    Create Reversenumberdecrement.java

---
+++ Reversenumberdecrement.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class patternprinting 
+{
+    public static void main(String args[])
+    {
+        int num=15;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\n Enter the number");
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n-i+1;j++)
+            {
+                 System.out.print(num + "  ");
+                 num--;
+            }
+            System.out.println();
+        }
+    }
+}
