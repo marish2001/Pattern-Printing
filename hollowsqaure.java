832220de7cc788cbb78824dcbb14218d071fbbda
M ANBU MARISH <113194705+marish2001@users.noreply.github.com>
2026-08-19 02:04:21 -0700

    Create hollowsqaure.java

---
+++ hollowsqaure.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class patternprinting 
+{
+    public static void main(String args[])
+    {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("\n Enter the number");
+       int n = sc.nextInt();
+
+       // for upper half
+       for(int i=0;i<=n;i++)
+       {
+          for(int j=0;j<=n;j++)
+          {
+            if(i==0 || j==0 || i==n || j==n)
+            {
+                System.out.print( "*");
+            }
+            else 
+            {
+                System.out.print(" ");
+            }
+          }
+          System.out.println();
+       }
+
+    }
+}
