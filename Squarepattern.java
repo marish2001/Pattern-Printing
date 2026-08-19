aff4f138e5e44333d2e2ef1565e929147f5f2e53
M ANBU MARISH <113194705+marish2001@users.noreply.github.com>
2026-08-18 23:45:44 -0700

    Create Squarepattern.java

---
+++ Squarepattern.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class patternprinting 
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\n Enter the number");
+        int n = sc.nextInt();
+        // for rows
+        for(int i=1;i<=n;i++)
+        {
+            // for columns
+            for(int j=1;j<=n;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
