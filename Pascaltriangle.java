7d28ef00b0b1abd92564595426c053382027aa21
M ANBU MARISH <113194705+marish2001@users.noreply.github.com>
2026-08-19 00:41:06 -0700

    Create Pascaltriangle.java

---
+++ Pascaltriangle.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+public class patternprinting 
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\n Enter the number");
+        int n = sc.nextInt();
+        // for upper half
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // for lower half 
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
